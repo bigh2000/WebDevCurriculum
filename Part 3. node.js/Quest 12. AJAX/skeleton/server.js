82b9b1cd050b9a9dc4721acd9ba4e88da96d1c12
dkkim <dkkim.math@gmail.com>
2017-05-01 01:19:57 +0900

    clear Quest 12

--- Part 3. node.js/Quest 12. AJAX/skeleton/server.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/server.js
@@ -1,15 +1,39 @@
 var express = require('express'),
 	path = require('path'),
-	app = express();
+	app = express(),
+	fs = require('fs'),
+	bodyParser = require('body-parser');
 
 app.use(express.static('client'));
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 /* TODO: 여기에 처리해야 할 요청의 주소별로 동작을 채워넣어 보세요..! */
+app.get('/note', function (req, res) {
+	var id = req.query.id;
 
-var server = app.listen(8080, function () {
+	fs.readFile('note' + id + '.txt', function(err, data) {
+		var d;
+		if (err)
+			d = "";
+		else
+			d = data;
+		res.send(d);
+	});
+});
+
+app.post('/note', function (req, res) {
+	var id = req.query.id;
+	fs.writeFile('note' + id + '.txt', req.body['data'], "utf8", function (err) {
+		if (err) throw err;
+		console.log('note' + id + '.txt is saved');
+	});
+	res.send();
+});
+
+var server = app.listen(22221, function () {
 	console.log('Server started!');
 });
